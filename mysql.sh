00650743be7d5776fee1e163eaba0e7ab008fe3e
nandini965 <nandhini965@gmail.com>
2024-01-16 04:03:41 +0530

    filebeat

--- mysql.sh
+++ mysql.sh
@@ -4,15 +4,20 @@ source ${script_path}/common.sh
 
 if [ -z "${mysql_root_password}" ]; then
   echo Input mysql root password is missing
+  fi
 func_print_head "disable mysql"
 dnf module disable mysql -y &>>$log_file
+func_stat_check $?
 
 func_print_head "install mysql"
 dnf install mysql-community-server -y &>>$log_file
+func_stat_check $?
 
 func_print_head "restart mysql"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
+func_stat_check $?
 
 func_print_head "add user and password"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+func_stat_check $?
