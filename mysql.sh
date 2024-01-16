d17bbc43e1192764c9c886a805214874370262f3
nandini965 <nandhini965@gmail.com>
2024-01-16 17:57:11 +0530

    filebeat

--- mysql.sh
+++ mysql.sh
@@ -5,6 +5,7 @@ mysql_root_password=$1
 
 if [ -z "${mysql_root_password}" ]; then
   echo Input mysql root password is missing
+  exit 1
   fi
 func_print_head "disable mysql"
 dnf module disable mysql -y &>>$log_file
