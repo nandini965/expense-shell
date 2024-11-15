813042761e6bbde71cc31cdbe447d519e895286e
nandini965 <nandhini965@gmail.com>
2024-11-16 03:58:01 +0530

    filebeat

--- frontend.sh
+++ frontend.sh
@@ -4,12 +4,17 @@ source ${script_path}/common.sh
 
 func_print_head "install nginx"
 dnf install nginx -y &>>$log_file
+func_stat_check $?
+
+func_print_head " copy configuration file "
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+ func_stat_check $?
 
  func_print_head "remove application user"
  rm -rf /usr/share/nginx/html/* &>>$log_file
 
  func_print_head "download application file"
- curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip  &>>$log_file
 
  func_print_head "create application user"
  cd /usr/share/nginx/html &>>$log_file
