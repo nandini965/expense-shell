588a37edb05e80c25add132888d677a342f27626
nandini965 <nandhini965@gmail.com>
2024-11-16 04:33:31 +0530

    filebeat

--- backend.sh
+++ backend.sh
@@ -58,7 +58,7 @@ dnf install mysql-server -y &>>$log_file
 func_stat_check $?
 
 func_print_head "load schema"
-mysql -h 172.31.40.169 -uroot -p"${mysql_root_password}" < /app/schema/backend.sql &>>$log_file
+mysql -h 172.31.40.169 -uroot -p"$mysql_root_password" < /app/schema/backend.sql &>>$log_file
 func_stat_check $?
 
 
